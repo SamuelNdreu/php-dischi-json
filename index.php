8a0e8d6be43642886eb2260bc4cfa89c23c5169e
SamuelNdreu <samwellsoficcial@gmail.com>
2023-09-11 13:40:20 +0200

    printed disch images on page

--- index.php
+++ index.php
@@ -9,7 +9,32 @@
     <title>Php Disch</title>
 </head>
 <body>
-    
+    <div id="app">
+        <header>
+            <div class="container h-100">
+                <div class="row h-100">
+                    <div class="col-12 h-100 d-flex align-item-center">
+                        <img class="h-50" src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Spotify_logo_without_text.svg/800px-Spotify_logo_without_text.svg.png" alt="">
+                    </div>
+                </div>
+            </div>
+        </header>
+        <main>
+            <div class="container">
+                <div class="row justify-content-between">
+                    <div v-for="disco,index in dischiList" class="col-3 text-center text-white disco mx-3 my-3" @click="getMoreInfo(index)">
+                        <img class="w-100 p-5" :src=disco.poster :alt=disco.title>
+                        <p class="fw-bold m-2"> {{disco.title}} </p>
+                        <p class="m-2"> {{disco.author}} </p>
+                        <p class="fw-bold m-2"> {{disco.year}} </p>
+                </div>
+            </div>
+        </main>
+
+
+    </div>
+
+
     <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
     <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
     <script src="./js/main.js"></script>
